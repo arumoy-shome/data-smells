122251840dcb08e869ec2b4b16b2dee2def34216
Arumoy Shome <contact@arumoy.me>
2025-07-05 16:57:11 +0200

    Init airbnb analysis

--- data.py
+++ data.py
@@ -5,6 +5,7 @@ from kagglehub import KaggleDatasetAdapter
 REGISTRY = {
     "abalone": ("rodolfomendes/abalone-dataset/versions/3", "abalone.csv"),
     "adult": ("uciml/adult-census-income/versions/3", "adult.csv"),
+    "airbnb": ("dgomonov/new-york-city-airbnb-open-data/versions/3", "AB_NYC_2019.csv"),
 }
 
 def load_dataset(name: str) -> pd.DataFrame:
